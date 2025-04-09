40546ee76f67b17519ba8c17eb7654edaf318605
April Hall <ari@arithefirst.com>
2025-04-09 12:35:59 -0400

    style: Adjust prettier config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import * as Card from '$lib/components/ui/card/index.js';
-	import { Textarea } from '$lib/components/ui/textarea/index.js';
-	import { Button } from '$lib/components/ui/button/index.js';
+  import * as Card from '$lib/components/ui/card/index.js';
+  import { Textarea } from '$lib/components/ui/textarea/index.js';
+  import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<Card.Root class="border-primary mx-auto mt-8 w-11/12 md:w-1/2">
-	<form>
-		<Card.Header>
-			<Card.Title>Create Post</Card.Title>
-		</Card.Header>
-		<Card.Content>
-			<Textarea name="content" id="content" placeholder="Share your thoughts with the world!" />
-		</Card.Content>
-		<Card.Footer>
-			<Button type="submit" class="cursor-pointer">Post!</Button>
-		</Card.Footer>
-	</form>
+<Card.Root class="border-primary mx-auto mt-8 w-11/12 rounded-lg md:w-1/2">
+  <form>
+    <Card.Header>
+      <Card.Title>Create Post</Card.Title>
+    </Card.Header>
+    <Card.Content>
+      <Textarea name="content" id="content" placeholder="Share your thoughts with the world!" />
+    </Card.Content>
+    <Card.Footer>
+      <Button type="submit" class="cursor-pointer">Post!</Button>
+    </Card.Footer>
+  </form>
 </Card.Root>
