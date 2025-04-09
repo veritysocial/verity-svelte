743666ba039fe3da8ab187417b9e96d5aa7085ae
April Hall <ari@arithefirst.com>
2025-04-09 12:24:11 -0400

    feat: Post creation form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<Card.Root class="mx-auto mt-8 w-11/12 md:w-1/2">
+	<form>
+		<Card.Header>
+			<Card.Title>Create Post</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<Textarea name="content" id="content" placeholder="Share your thoughts with the world!" />
+		</Card.Content>
+		<Card.Footer>
+			<Button type="submit" class="cursor-pointer">Post!</Button>
+		</Card.Footer>
+	</form>
+</Card.Root>
