ae17a2a9b4d997de99fa6c521d81c1b39854e4e4
April Hall <ari@arithefirst.com>
2025-04-09 16:08:53 -0400

    feat: Logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,22 @@
   import { ClerkProvider, SignedIn, SignedOut, SignUpButton, UserButton } from 'svelte-clerk';
   import { buttonVariants } from '$lib/components/ui/button';
   import { useClerkAppearance } from '$lib/utils/clerkTheme';
+  import Logo from '$lib/components/logo.svelte';
 
   const { children }: { children: Snippet } = $props();
 </script>
 
 <ClerkProvider appearance={useClerkAppearance()}>
+  <div class="text-foreground absolute top-4 right-4 flex flex-col items-end">
+    <Logo />
+    <p class="w-fit">
+      by <a
+        href="https://www.arithefirst.com/"
+        class="text-primary hover:text-primary/90 mr-2 underline"
+        target="_blank">April Hall</a
+      >
+    </p>
+  </div>
   <nav class="absolute top-4 left-4">
     <SignedOut>
       <SignUpButton mode="modal" appearance={useClerkAppearance()}>
